94f9aae8ddbdb4bdee9d9b5a69a45868843badfe
Santiago Sáchica <ssachica@gmail.com>
2023-11-10 16:37:52 -0500

    Reto #7: El sombrero seleccionador Completo - Minor bug fixes about words badly written

---
+++ src/main/kotlin/challenges/mouredev/GeneradorPseudoaleatorio.kt
@@ -0,0 +1,13 @@
+package challenges.mouredev
+
+/*
+ * Reto #8: El generador pseudoaleatorio
+ * Crea un generador de números pseudoaleatorios entre 0 y 100.
+ * - No puedes usar ninguna función "random" (o semejante) del lenguaje de programación seleccionado.
+ *
+ * Es más complicado de lo que parece...
+ */
+
+fun main(args: Array<String>) {
+
+}
